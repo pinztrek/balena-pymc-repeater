13f60fac100455071e060d328f23c767097c03a2
Alan Barrow <jab@EB840-G1.tail89489a.ts.net>
2026-06-11 22:10:35 -0400

    reduce sleep for now

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,12 +4,14 @@ echo "docker-entrypoint.sh started"
 # Change ownership of GPIO devices to the 'gpio' group
 # and grant read/write access to that group.
 if [ -e /dev/gpiochip0 ]; then
+    echo "override gpio groups..."
     sudo chgrp gpio /dev/gpiochip*
     sudo chmod g+rw /dev/gpiochip*
 fi
 
 # If you also need access to gpiomem
 if [ -e /dev/gpiomem ]; then
+    echo "override gpiomem groups..."
     sudo chgrp gpio /dev/gpiomem
     sudo chmod g+rw /dev/gpiomem
 fi
@@ -18,7 +20,7 @@ ls -al /dev/gpi*
 
 # Seed the radio settings if missing
 if [ ! -f /var/lib/pymc_repeater/radioi-settings.json ]; then
-    echo "Install radio  files..."
+    echo "Install radio files..."
     sudo cp /opt/pymc_repeater/radio* /var/lib/pymc_repeater
     sudo chown repeater:repeater /var/lib/pymc_repeater/radio*
 fi
@@ -34,5 +36,5 @@ cd /etc/pymc_repeater
 echo "docker-entrypoint.sh starting app"
 # Now run the application
 #exec "$@"
-pymc-repeater ; sleep 300
+pymc-repeater ; sleep 30
 echo "docker-entrypoint.sh exit"
